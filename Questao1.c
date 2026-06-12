a0d7dc631a8f0bc4230129ed6cabbea07a220b44
Luiz Sales <luiz.salessi2025@gmail.com>
2026-06-11 21:52:43 -0300

    Create Questao1.c

---
+++ Questao1.c
@@ -0,0 +1,5 @@
+B) A instrução p = q; faz com que p passe a apontar para o mesmo lugar que q. Ou seja, p deixa de apontar para a e passa a apontar para b. Assim, ao usar p, você estará alterando o valor de b. 
+
+C) A = 15 
+
+B = 10 
